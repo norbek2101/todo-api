4498aeaf52e4b5c91bcaf4e888fc676a7891d7c0
norbek <lightnight1223@gmail.com>
2021-10-30 02:11:56 +0500

    :

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'todo',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
