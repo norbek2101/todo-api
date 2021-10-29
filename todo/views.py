a5ebafa0ab3fa39aedc7ff7e1450ee63ad13be5e
norbek <lightnight1223@gmail.com>
2021-10-30 02:46:46 +0500

    Task view class added to views.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from .models import Task
+from .serializers import TodoSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class TaskListView(APIView):
+    
+    def get(self, request):
+        task = Task.objects.all()
+        serializer = TodoSerializer(task, many=True)
+        return Response(serializer.data)
+
+    
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class TaskDetailView(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TodoSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TodoSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
